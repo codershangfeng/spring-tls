7f0de9ce1f4f442b1bcdf66b7ef06dbdaa15bd7f
Shangfeng Liu <codershangfeng@gmail.com>
2021-04-30 19:03:18 +0800

    init

---
+++ src/main/kotlin/com/example/springtls/SpringTlsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springtls
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringTlsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringTlsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springtls/SpringTlsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springtls
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTlsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
